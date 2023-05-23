1bcecc422a19bcbff216ea2db309a25aa89d78f5
Remy Duijsens <remyduijsens@gmail.com>
2023-05-23 18:58:45 +0200

    Improve model-service by using the bow model directly<

--- webservice.py
+++ webservice.py
@@ -1,7 +1,5 @@
 import os
-
-import pipeline.preprocess as preprocessor
-import pipeline.test as model
+import pipeline.model as model
 from flask import Flask, request
 
 app = Flask(__name__)
@@ -13,8 +11,7 @@ EXPOSE_PORT = os.environ.get("EXPOSE_PORT", 80)
 def predict():
     """ Create prediction page """
     input_line = request.get_json().get('input')
-    preprocessed_input = preprocessor.load_single(input_line)
-    prediction = model.predict(preprocessed_input)
+    prediction = model.predict(input_line)
 
     return {
         "prediction": prediction[0].item()
